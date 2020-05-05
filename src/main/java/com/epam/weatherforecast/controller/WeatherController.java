c164be99878df7c660189ef9e41b570e8b3be5d0
Henrique Luz <domingos_luz@epam.com>
2020-05-05 11:27:50 +0200

    Simple controller responsible to handle REST layer

---
+++ src/main/java/com/epam/weatherforecast/controller/WeatherController.java
@@ -0,0 +1,24 @@
+package com.epam.weatherforecast.controller;
+
+import com.epam.weatherforecast.model.Weather;
+import com.epam.weatherforecast.service.ForecastService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WeatherController {
+
+    private ForecastService service;
+
+    public WeatherController(ForecastService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/weather")
+    public ResponseEntity<Weather> getCurrentWeather(@RequestParam("city") String city) {
+        return ResponseEntity.ok(service.getCurrentWeatherByCity(city));
+    }
+
+}
