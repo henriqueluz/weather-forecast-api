69fe43622f7a9bcca6427bf32989eae1ae14eeb2
Henrique Luz <domingos_luz@epam.com>
2020-05-05 15:55:46 +0200

    Customizing Swagger

--- src/main/java/com/epam/weatherforecast/controller/WeatherController.java
+++ src/main/java/com/epam/weatherforecast/controller/WeatherController.java
@@ -3,6 +3,8 @@ package com.epam.weatherforecast.controller;
 import com.epam.weatherforecast.exception.EntityNotFoundException;
 import com.epam.weatherforecast.model.Weather;
 import com.epam.weatherforecast.service.ForecastService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,13 +15,17 @@ public class WeatherController {
 
     private ForecastService service;
 
-    public WeatherController(ForecastService service) {
-        this.service = service;
-    }
-
     @GetMapping("/weather")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Get current weather for a given city"),
+            @ApiResponse(code = 404, message = "City not found"),
+    })
     public ResponseEntity<Weather> getCurrentWeather(@RequestParam("city") String city) throws EntityNotFoundException {
         return ResponseEntity.ok(service.getCurrentWeatherByCity(city));
+    }
+
+    public WeatherController(ForecastService service) {
+        this.service = service;
     }
 
 }
